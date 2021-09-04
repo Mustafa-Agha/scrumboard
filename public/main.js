6098c62aa225b89bbe0a4ff6eb89e11ea4ae0098
Mustafa-Agha <mustafaagha.developer@gmail.com>
2021-09-04 19:35:46 +0200

    fixed minor bug

--- public/main.js
+++ public/main.js
@@ -25,7 +25,7 @@ function createWindow() {
   win.loadURL(
     isDev
       ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '.../build/index.html')}`,
+      : `file://${path.resolve(__dirname, '../build/', 'index.html')}`,
   );
 }
 
